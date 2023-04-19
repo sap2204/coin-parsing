0f1132a8b8e2f76e09805fbffdb09806dc2288e3
Sergei Pokhorukov <tbksap@gmail.com>
2023-04-19 23:13:45 +0300

    Was created icon and background of programm window

--- main.py
+++ main.py
@@ -5,18 +5,27 @@ root = tk.Tk()
 
 root.title('Парсинг монет')
 
-# Создание ширины и высоты окна
-w = 800
-h = 600
+#Задание размеров окна программы
+w = 600
+h = 300
 
-# Определение ширины и высоты экрана пользователя
+#Определение разрешения экрана
 ws = root.winfo_screenwidth()
-wh = root.winfo_height()
+wh = root.winfo_screenheight()
 
-# Определение верхнего левого угла программы
+#Определение верхнего левого угла окна программы
 x = int(ws/2 - w/2)
 y = int(wh/2 - h/2)
 
-# Вывод окна программы по центру экрана
-root.geometry("{0} x {1} + {2} + {3}".format(w, h, x, y))
+# Выравнивание окна программы по центру экрана
+root.geometry("{0}x{1}+{2}+{3}".format(w, h, x, y))
+
+# Установка иконки в окне программы
+root.iconbitmap('icon.ico')
+
+# Установка фона программы
+bgimage = tk.PhotoImage(file = 'background.png')
+backgr = tk.Label(root, image = bgimage)
+backgr.pack()
+
 root.mainloop()
