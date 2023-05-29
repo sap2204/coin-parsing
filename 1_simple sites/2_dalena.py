c2a2765951b6a4e84a7671fa9c5343ee0646d5ba
Sergei Pokhorukov <tbksap@gmail.com>
2023-05-29 20:47:30 +0300

    Dalena bank was parced

--- 1_simple sites/2_dalena.py
+++ 1_simple sites/2_dalena.py
@@ -50,7 +50,44 @@ if responce.ok == True:
         logger.info('Получено название монеты {0} '.format(coin_name))
 
         # Масса монеты
-        weight = coin.findAll('div', class_ = 'bx-newslist-other')[0].text.strip().replace(' ', '')
+        weight = coin.findAll('div', class_ = 'bx-newslist-other')[0].text.strip().replace(' ', '').replace(',', '.')
         weight_coin = re.search(weight_pattern, weight).group(0)
         logger.info('Получен вес монеты {0} '.format(coin_name))
 
+        # Металл монеты
+        me = coin.findAll('div', class_ = 'bx-newslist-other')[1].text.strip().lower()
+        coin_me = re.search(me_pattern, me).group(0)  + ' ' + re.search(r'\d+', me).group(0) + '-' # добавление разделителя "-"
+        logger.info('Получен металл монеты {0} '.format(coin_name))
+
+        # Номинал монеты
+        nominal = coin.findAll('div', class_ = 'bx-newslist-other')[2].text.strip()
+        coin_nominal = re.search(nominal_pattern, nominal).group(0) + '-' # добавление разделителя '-'
+        logger.info('Получение номинала {0} '.format(coin_name))
+
+        # Цена монеты
+        price = coin.findAll('div', class_ = 'bx-newslist-other')[3].text.strip().replace(' ', '')
+        coin_price = re.search(price_pattern, price).group(0)
+        logger.info('Получение цены {0} '.format(coin_name)) 
+
+        # Добавление элементов в список
+        data.append([coin_name, '', coin_price, weight_coin, coin_me, coin_nominal])
+        logger.info('Добавление параметров монеты в список')
+
+# Создание колонок для датафрейма
+titles = ['Монета','Ном.- Проба - Вес', 'Цена', 'Вес', 'Металл', 'Номинал' ]
+
+# Создание и формирование итогового вида датафрейма
+df = pd.DataFrame(data, columns = titles)
+df['Ном.- Проба - Вес'] = df['Номинал']  + df['Металл'] + df['Вес'] # создание колонки с описанием монеты
+df.drop(columns = ['Номинал', 'Металл', 'Вес'], inplace = True)# удаление ненужных колонок
+logger.info('Создан итоговый датафрейм')
+
+# Занесение датафрейма в таблицу через xlsxwriter
+
+with pd.ExcelWriter('{}.xlsx'.format(bank_name), engine = 'xlsxwriter') as writer:
+    df.to_excel(writer, sheet_name = bank_name, index = False)
+    
+    sheet = writer.sheets[bank_name]
+    sheet.set_column('A:B', 80) # установка ширины столбца "Монета"
+    sheet.set_column('B:B', 30) # установка ширины столбца "Ном.- Проба - Вес"
+logger.info('Создана таблица {}.xlsx '.format(bank_name) )
