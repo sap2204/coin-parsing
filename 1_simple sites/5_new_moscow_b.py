3afd8a91dbe5645d4abe73cb2f2ac9a380c63129
sap2204 <tbksap@gmail.com>
2023-06-01 14:36:26 +0300

    New Moscow bank was added. Neet to finish, but I have a problem. Sssuuuka

--- 1_simple sites/5_new_moscow_b.py
+++ 1_simple sites/5_new_moscow_b.py
@@ -50,7 +50,48 @@ if responce.ok == True:
         coin_name = coin.find('strong', class_ = 'title add-color').text.strip()
         logger.info('Получено название монеты {0} '.format(coin_name))
 
+        # Описание монеты
+        description = coin.findAll('div', class_ = 'col')[1].find('p').text.replace(' ', '').replace(',', '.').replace('-', '')
+        #print('Полное описание ----',description)
+        # Получаем из описания только список цифр
+        only_numb = re.findall(r'\d+', description)
+        #logger.info('Получен список цифрового описания монеты')
+        if only_numb[0] == '1': # Удалил лишний нулевой элемент  в монете 'Веселая карусель'
+            del only_numb[0]
+        #print(only_numb)
+
+        # Для определения номинала и веса из списка цифр ищем числа из 1 и 2-х цифр (х и хх)
+        # Создаем список для одно и двузначных чисел, чтобы из него забрать номинал и вес
+        nom_weight_list = []
+        number_3_list = [] # список для хранения пробы металла
+        for i in only_numb:
+            number1_2 = ((re.search(r'\b\d{1,2}\b', i))) # поиск 1 и 2-значных чисел
+
+            # поиск 3-значных чисел для пробы металла
+            number_3 = (re.search(r'\b\d{3}\b', i)) 
+            if number_3:
+                number_3 = (re.search(r'\b\d{3}\b', i)).group(0) + '-'
+                #print('Проба ', number_3 )
+
+                 # Металл монеты
+                coin_me = re.search(me_pattern, description).group(0) + ' ' + number_3
+                print(coin_me)
+                
+            if number1_2: # Если регулярное выражение нашло совпадение по шаблону
+                nom_weight_list.append(number1_2.group(0))
+        #print(nom_weight_list)
+        #logger.info('Получен список, из которого берем номинал и вес монеты')
+
         # Масса монеты
-        weight = coin.findAll('div', class_ = 'col')[1].find('p').text.replace(',', '.')
-        #coin_weight = re.search(weight_pattern, weight).group(0)
-        print(weight)
+        weight_coin = '{0}.{1}'.format(nom_weight_list[1], nom_weight_list[2])
+        #logger.info('Полученна МАССА монеты {0} '.format(coin_name))
+
+        # Металл монеты
+        
+        
+        
+        
+
+
+        
+        
